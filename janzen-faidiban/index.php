d84daa1fd53f1035d7fce2c5ebe45a9a2ef50569
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-03-12 11:24:36 +0900

    update janzen-faidiban

--- janzen-faidiban/index.php
+++ janzen-faidiban/index.php
@@ -1,5 +1,19 @@
 <?php 
 
     echo 'JANZEN FAIDIBAN';
+    echo '<br>';
+
+    $students = array(
+                        "Gloria", 
+                        "Sonny", 
+                        "Jou",
+                        "Yekson",
+                        "Wihelmina",
+                        "Paul"
+                    );
+
+    foreach($students as $student){
+        echo $student . "<br>";
+    }
     
 ?>
